4c008cc9dfc42c96959ad91de251a461c69bb394
DayanMonteiro <dmsmamb@gmail.com>
2023-11-03 17:46:26 -0300

    logic for registering email at the event

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "60c89b52dde40937"; }
+/******/ 		__webpack_require__.h = function() { return "5f693b396e634ede"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
